03c18bc4d7b2d49c932cbb6f6bc82cf1a34d0357
jimenagr <a01252605@itesm.mx>
2024-04-08 09:52:22 -0600

    HomePage - search bar

---
+++ app/components/HomepageCards.tsx
@@ -0,0 +1,219 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import { Dialog, TextField, Button, Flex, Text } from "@radix-ui/themes";
+
+function HomepageCards({
+  keyword,
+  specificArea,
+}: {
+  keyword: string;
+  specificArea: string;
+}) {
+  const data = [
+    {
+      name: "Social Networking",
+      image: "/areas/social_network.jpeg",
+      alt: "Social Networking",
+      area: "Espacios Abiertos",
+      link: "/social-networking",
+      descripcion:
+        "Acceso abierto para colaborar e interactuar con un videowall interactivo. No requiere registro previo.",
+      id: 1,
+    },
+    {
+      name: "Lego Room",
+      image: "/areas/lego_room.jpeg",
+      alt: "Lego Room",
+      area: "Espacios Abiertos",
+      link: "/lego-room",
+      descripcion:
+        "Espacio creativo para estudiantes del área, con reserva previa para construir y diseñar con bloques Lego.",
+      id: 2,
+    },
+    {
+      name: "Electric Garage",
+      image: "/areas/electric_garage.jpeg",
+      alt: "Electric Garage",
+      area: "Garage Valley",
+      link: "/electric-garage",
+      descripcion:
+        "Taller equipado para proyectos de robótica e ingeniería para estudiantes de IRS.",
+      id: 3,
+    },
+    {
+      name: "Dimension Forge",
+      image: "/areas/dimension_forge.jpeg",
+      alt: "Dimension Forge",
+      area: "Garage Valley",
+      link: "/dimension-forge",
+      descripcion:
+        "Estudio avanzado para creación en 3D con impresoras y escáneres de última generación.",
+      id: 4,
+    },
+    {
+      name: "New Horizons",
+      image: "/areas/new_horizons.jpeg",
+      alt: "New Horizons",
+      area: "Garage Valley",
+      link: "/new-horizons",
+      descripcion:
+        "Espacio de realidad virtual y aumentada con instalación de Holodeck para inmersión total.",
+      id: 5,
+    },
+    {
+      name: "Deep Net",
+      image: "/areas/deep_net.jpeg",
+      alt: "Deep Net",
+      area: "Garage Valley",
+      link: "/deep-net",
+      descripcion:
+        "Zona para pruebas de infraestructura de redes, ideal para proyectos de investigación y desarrollo.",
+      id: 6,
+    },
+    {
+      name: "Graveyard",
+      image: "/areas/graveyard.jpeg",
+      alt: "Graveyard",
+      area: "Garage Valley",
+      link: "/graveyard",
+      descripcion:
+        "Espacio de reutilización de equipo tecnológico para ensamblar y reparar computadoras y servidores.",
+      id: 7,
+    },
+    {
+      name: "PCB Factory",
+      image: "/areas/pcb_factory.jpeg",
+      alt: "PCB Factory",
+      area: "Garage Valley",
+      link: "/pcb-factory",
+      descripcion:
+        "Equipado para la creación y montaje de circuitos impresos, localizado en CETEC para amantes de la electrónica.",
+      id: 8,
+    },
+    {
+      name: "The Matrix",
+      image: "/areas/the_matrix.webp",
+      alt: "The Matrix",
+      area: "Garage Valley",
+      link: "/the-matrix",
+      descripcion:
+        "Centro tecnológico para acceso a software especializado, máquinas virtuales, y bases de datos.",
+      id: 9,
+    },
+    {
+      name: "Hack-Battlefield",
+      image: "/areas/hack_battlefield.jpeg",
+      alt: "Hack-Battlefield",
+      area: "Zona de X-Ploración",
+      link: "/hack-battlefield",
+      descripcion:
+        "Espacio especializado para alumnos de ciberseguridad, equipado para prácticas y desarrollos en seguridad informática.",
+      id: 10,
+    },
+    {
+      name: "Testing-Land",
+      image: "/areas/testing_land.webp",
+      alt: "Testing-Land",
+      area: "Zona de X-Ploración",
+      link: "/testing-land",
+      descripcion:
+        "Área preparada con cámaras y audio para pruebas de usabilidad y compatibilidad en software, con enfoque en UX/UI, ideal para ITC e ITD.",
+      id: 11,
+    },
+    {
+      name: "War Headquarter",
+      image: "/areas/war_headquarters.png",
+      alt: "War Headquarter",
+      area: "Zona de X-Ploración",
+      link: "/war-headquarter",
+      descripcion:
+        "Sala de juntas diseñada para emular los centros de decisión ejecutiva, destinada a estrategias y deliberaciones críticas.",
+      id: 12,
+    },
+    {
+      name: "Biometrics Flexible ",
+      image: "/areas/biometrics_flexible_hall.png",
+      alt: "Biometrics Flexible Hall",
+      area: "Zona de X-Ploración",
+      link: "/biometrics-flexible-hall",
+      descripcion:
+        "Amplio salón con tecnología biométrica avanzada, adaptable para hackatones y observación, capaz de subdividirse para eventos menores.",
+      id: 13,
+    },
+    {
+      name: "Beyon-Digits",
+      image: "/areas/beyon_digits.jpeg",
+      alt: "Beyon-Digits",
+      area: "Zona de X-Ploración",
+      link: "/beyon-digits",
+      descripcion:
+        "Ambiente tecnológico con videowalls para visualización y trabajo colaborativo en proyectos digitales.",
+      id: 14,
+    },
+    {
+      name: "Open Innovation Lab",
+      image: "/areas/open_innovation_lab.webp",
+      alt: "Open Innovation Lab",
+      area: "Zona de X-Ploración",
+      link: "/open-innovation-lab",
+      descripcion:
+        "Espacio reservado para docentes y staff enfocado en la investigación, disponible para reservas exclusivas por parte del profesorado.",
+      id: 15,
+    },
+  ];
+  const filteredData = data.filter((item) => {
+    const nameMatch = item.name.toLowerCase().includes(keyword.toLowerCase());
+    const areaMatch =
+      specificArea.trim() === "" ||
+      item.area.toLowerCase() === specificArea.toLowerCase();
+
+    if (keyword.trim() === "") {
+      return areaMatch;
+    } else {
+      return nameMatch && areaMatch;
+    }
+  });
+
+  return (
+    <div className="flex justify-center">
+      <div className="w-[90%] lg:w-[65.4%] flex flex-wrap justify-start">
+        {filteredData.map((item) => (
+          <Dialog.Root>
+            <Dialog.Trigger>
+              <div key={item.id}>
+                <div className="m-4">
+                  <Image
+                    src={item.image}
+                    alt={item.alt}
+                    height={200}
+                    width={200}
+                    className="rounded-lg object-cover h-28 w-36 lg:h-40 lg:w-52"
+                  />
+                  <div className="text-sm lg:text-xl font-semibold tracking-wide ">
+                    {item.name}
+                  </div>
+                  <div className="text-xs lg:text-base font-light tracking-wide text-slate-400">
+                    {item.area}
+                  </div>
+                </div>
+              </div>
+            </Dialog.Trigger>
+
+            <Dialog.Content>
+              {item.descripcion}
+              <Dialog.Close>
+                <Button variant="soft" color="gray">
+                  Cancel
+                </Button>
+              </Dialog.Close>
+            </Dialog.Content>
+          </Dialog.Root>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default HomepageCards;
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,135 +1,54 @@
-"use client"
-import { motion } from "framer-motion";
-import { useState, useEffect } from "react";
-import axios from "axios";
-import { TypeAnimation } from "react-type-animation";
-import { ClipLoader } from "react-spinners";
-import Cookies from 'js-cookie'
-import { Button, TextFieldInput, TextFieldRoot, TextFieldSlot } from "@radix-ui/themes";
-import Link from "next/link";
+"use client";
+import React, { useState } from "react";
+import HomepageCards from "./components/HomepageCards";
+import { Tabs, Box, Text } from "@radix-ui/themes";
 
-
-export default function Home() {
-  const [npcText, setNpcText] = useState("¿Hola!, ¿Cómo te puedo ayudar?")
-  const [playerText, setPlayerText] = useState("")
-  const [loading, setLoading] = useState(false)
-  const [room, setRoom] = useState("bg2")
-  const [id, setId] = useState("1")
-  const [interactions, setInteractions] = useState(0)
-
-  const handleKeyPress = (e: any) => {
-    if (e.key === 'Enter') {
-      handleSend();
-    }
-  }
-
-  useEffect(() => {
-    const token = Cookies.get("id")
-    setId(token ?? "1")
-    console.log(token)
-  }, [])
-
-  const handleSend = async () => {
-    setLoading(true)
-    try {
-      const response = await axios.post("/api/chat", {
-        playerText: playerText,
-        id: id
-      })
-      const room = checkForRoom(response.data.npcText.response)
-      if (room) {
-        setRoom(room)
-        response.data.npcText.response = response.data.npcText.response.split("$$")[2]
-        console.log(response.data.npcText.response)
-      }
-      setPlayerText("")
-      setInteractions(interactions + 1)
-      setNpcText(response.data.npcText.response)
-      setLoading(false)
-    } catch (e) {
-      console.log(e)
-      setLoading(false)
-    }
-  }
-
-  
-
-  const checkForRoom = (text: string) => {
-    if (text && text.includes("$$")) {
-      const char = text.split("$$")[1]
-      return char
-    }
-  }
-
+function page() {
+  const [name, setName] = useState("");
   return (
-    <div className="bg-[#121417] h-screen flex flex-col overflow-hidden relative">
-      {room !== "" && <img src={`/${room}.png`} className="w-full h-full object-cover z-0 absolute inset-0"/>}
-        <div className="w-full h-[85%] relative">
-          <div className="h-[40%] w-full flex items-center justify-center gap-32 z-10">
-            <motion.div 
-              className="bg-[#121417] border-[#42454A] border-2 h-[175px] w-[225px] rounded-[30px] flex justify-center px-5 items-center z-10"
-              initial={{opacity: 0}}
-              animate={{opacity: 1}}
-              transition={{duration: 1, delay: 3}}>
-                {playerText}
-              </motion.div>
-              <motion.div 
-              className="bg-[#121417] border-[#42454A] border-2 h-[175px] w-[300px] rounded-[30px] flex justify-center px-5 py-4 items-center overflow-y-auto z-10"
-              initial={{opacity: 0}}
-              animate={{opacity: 1}}
-              transition={{duration: 1, delay: 3}}>
-                {!loading ? <TypeAnimation sequence={[npcText]} speed={75}></TypeAnimation>: <ClipLoader />}
-              </motion.div>
+    <div className=''>
+      <div className="flex justify-center ">
+        <input
+          value={name}
+          onChange={e => setName(e.target.value)}
+          placeholder= "Busca un area"
+          className="flex justify-center w-2/5 p-2.5 rounded-lg text-sm bg-[#293038]  "
+          //className="bg-[#293038] border border-gray-300 text-gray-900 text-sm rounded-lg block w-1/2 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        />
+      </div>
+      <div className=''> 
+        <Tabs.Root defaultValue="todos">
+          <div className="flex justify-center text-xl ">
+            <Tabs.List className="w-full lg:w-[65.4%] flex justify-center">
+              <Tabs.Trigger value="todos"> <Text size={{initial:"1", lg:"3"}}>Todos</Text> </Tabs.Trigger>
+              <Tabs.Trigger value="espacio_abierto"> <Text size={{initial:"1", lg:"3"}}>Espacios Abiertos</Text> </Tabs.Trigger>
+              <Tabs.Trigger value="garage_valley"> <Text size={{initial:"1", lg:"3"}}>Garage Valley</Text> </Tabs.Trigger>
+              <Tabs.Trigger value="zona_xploracion"> <Text size={{initial:"1", lg:"3"}}>Zona de Xploracion</Text> </Tabs.Trigger>
+            </Tabs.List>
           </div>
-          <div className="h-[60%] w-full flex items-end justify-center gap-24 z-10">
-            <motion.div 
-            className="h-[95%] w-[30%] rounded-full p-6 mb-6"
-            initial={{x: '-60vw'}}
-            animate={{x: 0}}
-            transition={{duration: 2}}>
-              <img src="/Usuario.png" className="w-[290px]"/>
-            </motion.div>
-            <motion.div 
-            className="h-[95%] w-[30%] rounded-full p-6 mb-6"
-            initial={{x: '60vw'}}
-            animate={{x: 0}}
-            transition={{duration: 2, delay: 1}}>
-              <img src="/robot.png" className="w-[300px]"/>
-            </motion.div>
-          </div>
-        </div>
-        <div className="w-full h-[15%] flex justify-center items-center bg-[#121417] bg-opacity-75 z-10">
-          <div className="w-full h-full flex justify-center items-center z-10">
-            <TextFieldRoot radius="full" size={"3"} style={{ display: 'flex', alignItems: 'center', width: '30vw', height: '7vh'}}>
-              <TextFieldInput 
-                placeholder="Escribe tu mensaje"
-                className=" bg-opacity-0 text-white"
-                radius="full"
-                value={playerText}
-                onChange={(e) => setPlayerText(e.target.value)}
-                onKeyDown={handleKeyPress}
-                style={{ marginRight: 'auto' }} // Align text field to the leftmost part
-              />
-              <button 
-                type="submit"  
-                className="z-10 hover:bg-opacity-60 transition-all bg-[#726FF5] rounded-full w-[8vw] h-[7vh] ml-auto text-xl flex items-center justify-center" 
-                onClick={handleSend}
-              >
-                {loading ? <ClipLoader size={20} color="white"/>: "Enviar"}
-              </button>
-            </TextFieldRoot>
-            <div>
-              {interactions > 3 && 
-                <Link href="/" className="text-neutral-400 ml-10 hover:text-white underline">
-                  Reservar manualmente
-                </Link>
-              }
-            </div>
-          </div>
-        </div>
+          <Box pt="3">
+            <Tabs.Content value="todos">
+              <HomepageCards keyword={name} specificArea="" />
+            </Tabs.Content>
+
+            <Tabs.Content value="espacio_abierto">
+              <HomepageCards keyword={name} specificArea="Espacios Abiertos" />
+            </Tabs.Content>
+
+            <Tabs.Content value="garage_valley">
+              <Text size="2">
+                <HomepageCards keyword={name} specificArea="Garage Valley" />
+              </Text>
+            </Tabs.Content>
+
+            <Tabs.Content value="zona_xploracion">
+              <HomepageCards keyword={name} specificArea="Zona de X-Ploración" />
+            </Tabs.Content>
+          </Box>
+        </Tabs.Root>
+      </div>
     </div>
   );
 }
 
-
-// rounded-full w-full px-3 h-full bg-[#726FF5] hover:bg-opacity-40 text-white text-lg flex items-center justify-center transition-all
+export default page;
