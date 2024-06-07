9778afb7d1b15795d6f400953ce2615349f63067
Luis Eduardo Garza Naranjo <luis.garzanaranjo@gmail.com>
2024-06-07 12:50:24 -0600

    admin update

--- app/utils/getrole.ts
+++ app/utils/getrole.ts
@@ -3,7 +3,7 @@ import axios from "axios"
 export const isAdmin = async () => {
     const res = await axios.get('/api/getRole')
     const data = res.data.data.payload.role
-    return data == "Admin"
+    return data == 7
 }
 
 export const getData = async () => {
