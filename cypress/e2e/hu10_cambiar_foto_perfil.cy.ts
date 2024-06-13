1abe490bf9d302987c8e7de55845a79de51be549
jimenagr <a01252605@itesm.mx>
2024-06-12 19:06:33 -0600

    pruebass

---
+++ cypress/e2e/hu10_cambiar_foto_perfil.cy.ts
@@ -0,0 +1,16 @@
+describe('Estadisticas Personal', () => {
+    it('Passed', () => {
+        cy.viewport(1440, 900);
+        cy.visit('https://dreamlab.azurewebsites.net/login');
+        cy.get(':nth-child(1) > .rt-TextFieldRoot > .rt-TextFieldInput').type('a01721881');
+        cy.get('.mt-4 > .rt-TextFieldRoot > .rt-TextFieldInput').type('hola123');
+        cy.contains('Iniciar sesión').click();
+        cy.contains('Sumérgete en la educación del futuro');
+        cy.get('.w-\\\[45px\\\]').click()
+        cy.get('.hover\\\:cursor-pointer').click()
+        cy.get(".h-64").selectFile("public/persona.jpeg");
+        cy.contains("Guardar").click()
+        cy.contains("Imagen guardada exitosamente")
+        cy.reload()
+    })
+})
