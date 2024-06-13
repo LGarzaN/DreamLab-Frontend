70546d408253a4d951d35ad59ed084579d17a449
Luis Eduardo Garza Naranjo <luis.garzanaranjo@gmail.com>
2024-06-13 11:58:29 -0600

    videowall fix

--- app/videowall/News.tsx
+++ app/videowall/News.tsx
@@ -55,7 +55,7 @@ const NewsCarrousel: React.FC<props> = ({newsArray}) => {
 const NewsCard = ({ news }: { news: News }) => {
     return (
         <div className="bg-white bg-opacity-10 border-gray-300  border-opacity-20 h-[60%] flex flex-row rounded-lg">
-            <img src={news.image || "/videowall/news1.png"} className="w-[700px] h-[350px]" alt="..." />
+            <img src={news.image || "/videowall/news1.png"} className="w-[700px] h-[350px] rounded-tl-lg rounded-bl-lg" alt="..." />
             <div className="w-[500px] flex flex-col px-10 py-5 text-center">
                 <p className="text-3xl font-bold mb-8">{news.title}</p>
                 <p className="text-xl text-left">{news.content}</p>  
